10b4e88d5e6c4c8dd034492a3605f7b46ea09283
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-29 03:38:37 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  #binding.pry
+  
+  
+  test1 = data[:gender][:male]
+  test2 = data[:gender][:female]
+  test3 = test2 + test1
+  
+  
+
+  
+  
+  new_hash = Hash[test3.map {|x| [x, Hash.new ]}]
+  
+  new_hash.each_key do |pigeon|
+    new_hash[pigeon] = Hash[data.keys.map {|x| [x, Array.new ]}]
+
+  end
+  
+  new_hash.each do |pigeon, attributes|
+    attributes.each do |attribute, values|
+      data[attribute].each do |value, pigeons|
+        if pigeons.index(pigeon)
+          values << String(value)
+          #binding.pry
+        end
+      end      
+    end
+  end  
+    
+
+  
+  binding.pry
+  
+  #new_hash.update(new_hash) { |key, value| value }
+
+  new_hash
+  
 end
