f895cfcc1573652f032b1c4b7019a656d3394505
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-30 19:52:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,9 +7,13 @@ def nyc_pigeon_organizer(data)
   
   test1 = data[:gender][:male]
   test2 = data[:gender][:female]
+<<<<<<< HEAD
   test3 = test2 + test1
   
   
+=======
+  test3 = test2.concat(test1)
+>>>>>>> 5a5cf0cae03ded396c9c5386143697372a3e661d
 
   
   
@@ -24,8 +28,13 @@ def nyc_pigeon_organizer(data)
     attributes.each do |attribute, values|
       data[attribute].each do |value, pigeons|
         if pigeons.index(pigeon)
+<<<<<<< HEAD
           values << String(value)
           #binding.pry
+=======
+          values << value
+          binding.pry
+>>>>>>> 5a5cf0cae03ded396c9c5386143697372a3e661d
         end
       end      
     end
@@ -33,7 +42,11 @@ def nyc_pigeon_organizer(data)
     
 
   
+<<<<<<< HEAD
   binding.pry
+=======
+  #binding.pry
+>>>>>>> 5a5cf0cae03ded396c9c5386143697372a3e661d
   
   #new_hash.update(new_hash) { |key, value| value }
 
